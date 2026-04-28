e94db1977195589116970270b4758a161b09d438
yjkim <planetes00@gmail.com>
2026-04-28 11:38:46 +0900

    Initial Commit

---
+++ WorkStopwatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WorkStopwatch
+//
+//  Created by 김예진 on 4/28/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkStopwatch/WorkStopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkStopwatchApp.swift
+//  WorkStopwatch
+//
+//  Created by 김예진 on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct WorkStopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
